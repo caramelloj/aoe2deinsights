d9459b64a6abdd37488773478cafbf94acab5e4e
caramelloj <caramellojose54@gmail.com>
2022-06-02 01:05:05 -0300

    added contentsecuritypolicy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        
         <title>{{ config('app.name', 'Argon Dashboard') }}</title>
         <!-- Favicon -->
         <link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
